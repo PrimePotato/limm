3abc48fb5f641b7179ba8ae734635db9d61d4ae9
Robert Cooper <RobertJohnCooper@Gmail.com>
2017-07-21 13:13:23 +0100

    0.0

--- graphic_detail.py
+++ graphic_detail.py
@@ -1,8 +1,13 @@
+import pygeoj
+
 from db_flask.manage import Disposal, Acquisition, AcquisitionArea
 from datetime import date
 import pandas as pd
 import numpy as np
 import geojson
+
+from land_registry import extract_postcode_stubs, extract_markets
+
 
 class DataProducer(object):
     def __init__(self, session):
@@ -70,75 +75,72 @@ class DataProducer(object):
             'web_data/disposal_average_rent_by_area_code.csv': self.disposal_average_rent_by_area_code()
         }
         for fn, df in reports.items():
-            print ('Saving {}'.format(fn))
+            print('Saving {}'.format(fn))
             df.to_csv(fn, header=True)
 
+    def average_cascade(self, column, area, district, sector, avg_area, avg_district, avg_sector):
+        if sector in avg_sector.index:
+            avg = avg_sector[column][sector]
+        elif district in avg_district.index:
+            avg = avg_district[column][district]
+        elif area in avg_area.index:
+            avg = avg_area[column][area]
+        else:
+            avg = self.df_disposals[column].mean()
+        if np.isnan(avg):
+            avg = self.df_disposals[column].mean()
+        return round(avg, 2)
 
-def average_cascade(column, area, district, sector, avg_area, avg_district, avg_sector):
-    if sector in avg_sector.index:
-        avg = avg_sector[column][sector]
-    elif district in avg_district.index:
-        avg = avg_district[column][district]
-    elif area in avg_area.index:
-        avg = avg_area[column][area]
-    else:
-        avg = df_disposals[column].mean()
-    if np.isnan(avg):
-        avg = df_disposals[column].mean()
-    return round(avg, 2)
+    def update_geojson_choropleth(self):
+        geo_data = pygeoj.load("central_london.geojson")
+        avg_area = self.df_disposals.groupby('area_code').mean()
+        avg_district = self.df_disposals.groupby('district_code').mean()
+        avg_sector = self.df_disposals.groupby('sector_code').mean()
 
+        for feature in geo_data:
+            ppt = feature.properties
 
-def update_geojson_choropleth():
-    geo_data = pygeoj.load("central_london.geojson")
-    avg_area = df_disposals.groupby('area_code').mean()
-    avg_district = df_disposals.groupby('district_code').mean()
-    avg_sector = df_disposals.groupby('sector_code').mean()
+            area, district, sector = extract_postcode_stubs(ppt['name'])
+            ppt['area'] = area
+            ppt['district'] = district
+            ppt['sector'] = sector
 
-    for feature in geo_data:
-        ppt = feature.properties
+            mkts = [m for m in extract_markets(ppt['name']) if m is not None]
+            ppt['sub_market'] = str(mkts[0])
+            ppt['wider_market'] = str(mkts[-1])
 
-        area, district, sector = extract_postcode_stubs(ppt['name'])
-        ppt['area'] = area
-        ppt['district'] = district
-        ppt['sector'] = sector
+            ppt.pop('column_1495195118190')
+            ppt.pop('cartodb_id')
 
-        mkts = [m for m in extract_markets(ppt['name']) if m is not None]
-        ppt['sub_market'] = str(mkts[0])
-        ppt['wider_market'] = str(mkts[-1])
+            ppt['average_rent'] = self.average_cascade('rent', area, district, sector, avg_area, avg_district,
+                                                       avg_sector)
+            ppt['average_rates'] = self.average_cascade('rates', area, district, sector, avg_area, avg_district,
+                                                        avg_sector)
+            ppt['average_size_min'] = self.average_cascade('size_min', area, district, sector, avg_area, avg_district,
+                                                           avg_sector)
+            ppt['average_size_max'] = self.average_cascade('size_max', area, district, sector, avg_area, avg_district,
+                                                           avg_sector)
+        geo_data.save("pyg_test.geojson")
 
-        ppt.pop('column_1495195118190')
-        ppt.pop('cartodb_id')
+    def update_geojson_heatmap(self):
+        output = pygeoj.new()
+        for idx, dsp in self.df_disposals.iterrows():
+            if not np.isnan(dsp.rent):
+                dsp.fillna(0., inplace=True)
+                pnt = geojson.Point((dsp.lng, dsp.lat))
+                ppt = {'size_max': dsp.size_max,
+                       'size_min': dsp.size_min,
+                       'rent': dsp.rent,
+                       'rates': dsp.rates}
+                print(ppt)
+                output.add_feature(properties=ppt, geometry=pnt)
 
-        ppt['average_rent'] = average_cascade('rent', area, district, sector, avg_area, avg_district, avg_sector)
-        ppt['average_rates'] = average_cascade('rates', area, district, sector, avg_area, avg_district, avg_sector)
-        ppt['average_size_min'] = average_cascade('size_min', area, district, sector, avg_area, avg_district, avg_sector)
-        ppt['average_size_max'] = average_cascade('size_max', area, district, sector, avg_area, avg_district, avg_sector)
+        output.add_unique_id()
+        output.save("point_heat.geojson")
 
-    geo_data.save("pyg_test.geojson")
-
-
-def update_geojson_heatmap():
-    output = pygeoj.new()
-    for idx, dsp in df_disposals.iterrows():
-        if not np.isnan(dsp.rent):
-            dsp.fillna(0., inplace=True)
-            pnt = geojson.Point((dsp.lng, dsp.lat))
-            ppt = {'size_max': dsp.size_max,
-                   'size_min': dsp.size_min,
-                   'rent':  dsp.rent,
-                   'rates': dsp.rates}
-            print(ppt)
-            output.add_feature(properties=ppt, geometry=pnt)
-        # break
-
-    output.add_unique_id()
-    output.save("point_heat.geojson")
-
-
-def disposal_scatter():
-    dfd = df_disposals[['my_hood', 'rent', 'rates', 'size_min', 'size_max']]
-    dfd.to_csv('dsp_data.csv')
-
+    def disposal_scatter(self):
+        dfd = self.df_disposals[['my_hood', 'rent', 'rates', 'size_min', 'size_max']]
+        dfd.to_csv('dsp_data.csv')
 
 # update_geojson_heatmap()
 # update_geojson_choropleth()
